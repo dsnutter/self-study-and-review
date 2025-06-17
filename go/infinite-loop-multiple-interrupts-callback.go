5c288f967935a67c99556c67e3ab4bf43944e41a
dsnutter <11504386+dsnutter@users.noreply.github.com>
2025-06-17 18:17:29 -0400

    addition of ctrl-z and other changes

--- go/infinite-loop-multiple-interrupts-callback.go
+++ go/infinite-loop-multiple-interrupts-callback.go
@@ -10,61 +10,56 @@ import (
 
 func main() {
 	var input string
-
 	inputInterrupt := map[string]os.Signal{
 		"c": syscall.SIGINT,
-		// 0x04 => Ctrl-D
-		"d": syscall.Signal(0x04),
 		"z": syscall.SIGTSTP,
 	}
 
-	fmt.Println("Capture which? Ctrl-C [c], Ctrl-D [d] or Ctrl-Z [z]?")
+	fmt.Println("Capture which? Ctrl-C [c], Ctrl-Z [z]?")
 	fmt.Scanf("%s", &input)
+	var desiredResult os.Signal = inputInterrupt["c"]
 
 	switch {
 	case input[0:1] == "C":
 	case input[0:1] == "c":
 		fmt.Println("Breaking out of loop on Ctrl-C")
-	case input[0:1] == "D":
-	case input[0:1] == "d":
-		fmt.Println("Breaking out of loop on Ctrl-D")
 	case input[0:1] == "Z":
 	case input[0:1] == "z":
 		fmt.Println("Breaking out of loop on Ctrl-Z")
+		desiredResult = inputInterrupt["z"]
 	default:
 		os.Exit(1)
 	}
 
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, inputInterrupt["c"], inputInterrupt["d"], inputInterrupt["z"])
+	signal.Notify(c, inputInterrupt["c"], inputInterrupt["z"])
+	var result os.Signal
+	var closeOk bool
 
-	// separate closeOk necessary due to closing the interrupt in the callback
-	closeOk := make(chan bool, 1)
+	go func() {
+		result, closeOk = <-c
+		if result == desiredResult && cap(c) >= 1 {
+			closeInterrupt(c)
+		}
+	}()
 
-	// concurrent thread with go keyword, with callback for closing
-	go interrupt(closeOk, c, closeInterrupt)
+	/*
+		// concurrent thread with go keyword
+		go func() {
+			result, closeOk = <-c
+
+			if closeOk && result == desiredResult && cap(c) >= 1 {
+				closeInterrupt(c)
+			}
+		}()
+	*/
 
 	for {
-		fmt.Println("infinite loop")
-
-		// if cap(closeOk) >= 1 {
-		// 	select {
-		// 	case ok := <-closeOk:
-		// 		if ok {
-		// 			close(closeOk)
-		// 			break
-		// 		}
-		// 	}
-		// }
-		if cap(closeOk) >= 1 {
-			for ok := range closeOk {
-				if ok {
-					close(closeOk)
-					break
-				}
-			}
+		time.Sleep(1 * time.Second)
+		if closeOk && cap(c) >= 1 {
+			break
 		} else {
-			time.Sleep(1 * time.Second)
+			fmt.Println("infinite loop")
 		}
 	}
 }
@@ -72,17 +67,4 @@ func main() {
 func closeInterrupt(c chan os.Signal) {
 	fmt.Println("Closing and stopping")
 	close(c)
-}
-
-func interrupt(closeOk chan bool, c chan os.Signal, callback func(c chan os.Signal)) {
-	var result os.Signal
-	var cOk bool
-	result, cOk = <-c
-
-	if cOk && result == os.Interrupt && cap(c) >= 1 {
-		callback(c)
-		go func() {
-			closeOk <- true
-		}()
-	}
 }
